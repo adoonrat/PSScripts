7a6a9a4ab4a3e13e205f365023e4e6fba5abdd1c
adoonrat <140907228+adoonrat@users.noreply.github.com>
2024-07-02 09:56:31 +0700

    Update WindowsUpdate.ps1

--- WindowsUpdate.ps1
+++ WindowsUpdate.ps1
@@ -6,15 +6,17 @@ if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
 # Import the PSWindowsUpdate module
 Import-Module PSWindowsUpdate
 
+$KB = "KB5039212"
+
 # Hide the progress UI
 $ProgressPreference = 'SilentlyContinue'
 
 # Search for available updates
-$Updates = Get-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
+$Updates = Get-WindowsUpdate -KBArticleID $KB -AcceptAll -IgnoreReboot
 
 # Install the available updates
 if ($Updates.Count -gt 0) {
-    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot -Install
+    Install-WindowsUpdate -KBArticleID $KB -AcceptAll -IgnoreReboot -Install -verbose
 }
 
 # Reboot the system if updates were installed
