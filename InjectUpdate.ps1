422fe47c8e4773fc5e8ab2b7827a94fa2645fefd
adoonrat <140907228+adoonrat@users.noreply.github.com>
2024-07-03 16:48:32 +0700

    Update InjectUpdate.ps1

--- InjectUpdate.ps1
+++ InjectUpdate.ps1
@@ -1,6 +1,6 @@
 $downloadLink = "https://catalog.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/a2fe5398-6f24-46ee-a533-372dc30bfd82/public/windows11.0-kb5039212-x64_2b67855a5e73c7a873e6bdca512c8c106b429196.msu"
 $DestinationName = $downloadLink.Split("/")[$downloadLink.Split("/").Count-1]
-$UpdateLocation = "C:\temp"
+$UpdateLocation = "W:\temp"
 
 If(!(Test-Path -Path "$UpdateLocation\$DestinationName"))
 {
